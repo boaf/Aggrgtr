012fe367505ef0794a433bab7a2deaf787e94ccb
Gibson Starkweather <gibson@gibson.fm>
2012-04-14 13:03:12 -0700

    bullshit

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "it works!";
+error_reporting(E_ALL);
+
+class Aggrgtr {
+    
+    private $username = "gibson";
+    private $count = 5;
+    private $url;
+    
+    function getUrl() {
+        $this->url = "http://twitter.com/statuses/user_timeline/" . $this->username . ".json?count=" . $this->count;
+        return $this->url;
+    }
+    
+}
+
+$aggrgtr = new Aggrgtr();
+echo $aggrgtr->getUrl();
